d17e6d1d2c23894f4988ecdff512748a198c41bc
Eric Rumble <eric.rumble@terminus.com>
2018-05-10 14:44:24 -0400

    refactor logic to empty bucket into it's own package

--- commands/empty_bucket.go
+++ commands/empty_bucket.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/GetTerminus/s3helper/lib/aws"
+	"github.com/GetTerminus/s3helper/lib/aws/s3svc"
 	"github.com/GetTerminus/s3helper/lib/parser"
-	"github.com/GetTerminus/s3helper/lib/provider"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/pkg/errors"
 )
@@ -34,74 +34,15 @@ func (cmd *EmptyBucketCommand) Execute(args []string) error {
 	// nolint [:gas]
 	fmt.Fprintf(os.Stdout, "Deleting contents of s3://%s\n", cmd.Bucket)
 
-	// Create a session and get an s3 handle
-	session, err := provider.AWSSession()
+	awsSession := aws.Client.GetSession()
+	s3client := s3svc.NewClient(s3.New(awsSession), parser.GlobalOpts.Verbose)
+
+	resp, err := s3client.DeleteBucketContents(cmd.Bucket)
 	if err != nil {
-		return errors.Wrap(err, "error getting aws session")
+		return errors.Wrap(err, "Package: commands => func: Execute => method call s3svc.Client.DeleteBucketContents failed\n")
 	}
 
-	svc := s3.New(session)
+	fmt.Println(resp)
 
-	totalDeleted := 0
-
-	for {
-		// 2 API calls are needed to delete things in s3, the first will get a list
-		// of the objects in the bucket, and the second to delete those things
-		resp, err := svc.ListObjectVersions(&s3.ListObjectVersionsInput{
-			Bucket: aws.String(cmd.Bucket),
-		})
-		if err != nil {
-			return errors.Wrap(err, "error listing object versions on bucket")
-		}
-
-		// svc.ListObjectVersions will only return 1000 results at a time
-		numObjects := len(resp.DeleteMarkers) + len(resp.Versions)
-		totalDeleted += numObjects
-
-		// delete things until there's nothing left
-		if numObjects == 0 {
-
-			// nolint [:gas]
-			fmt.Fprintf(os.Stdout, "Total number of objects deleted: %v\n", totalDeleted)
-			return nil
-		}
-
-		// We're emptying the bucket, no need to discrimiate the things to delete
-		var items s3.Delete
-		var objectIdentifiers = make([]*s3.ObjectIdentifier, numObjects)
-
-		for i, obj := range resp.Versions {
-			objectIdentifiers[i] = &s3.ObjectIdentifier{
-				Key:       obj.Key,
-				VersionId: obj.VersionId,
-			}
-		}
-
-		for i, obj := range resp.DeleteMarkers {
-			objectIdentifiers[i+len(resp.Versions)] = &s3.ObjectIdentifier{
-				Key:       aws.String(*obj.Key),
-				VersionId: aws.String(*obj.VersionId),
-			}
-		}
-
-		items.SetObjects(objectIdentifiers)
-
-		// nolint [:gas]
-		fmt.Fprintf(os.Stdout, "Deleting %v objects\n", numObjects)
-
-		if parser.GlobalOpts.Verbose {
-
-			// nolint [:gas]
-			fmt.Fprintln(os.Stdout, items)
-		}
-
-		// DeleteObjects can only delete 1000 things at a time
-		_, err = svc.DeleteObjects(&s3.DeleteObjectsInput{
-			Bucket: aws.String(cmd.Bucket),
-			Delete: &items,
-		})
-		if err != nil {
-			return errors.Wrap(err, "error deleting objects")
-		}
-	}
+	return nil
 }
--- lib/aws/s3svc/s3svc.go
+++ lib/aws/s3svc/s3svc.go
@@ -1,91 +1,103 @@
 package s3svc
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/pkg/errors"
 )
 
-type api interface {
+type API interface {
 	DeleteObjects(*s3.DeleteObjectsInput) (*s3.DeleteObjectsOutput, error)
-	ListObjects(*s3.ListObjectsInput) (*s3.ListObjectsOutput, error)
 	ListObjectVersions(*s3.ListObjectVersionsInput) (*s3.ListObjectVersionsOutput, error)
 }
 
-type client struct {
-	s3api   api
+type Client struct {
+	s3api   API
 	verbose bool
 }
 
 // NewClient returns a client struct that provides an interface to the s3 api.
-func NewClient(s3 api, v bool) *client {
-	c := new(client)
-	c.s3api = s3
-	c.verbose = v
-	return c
+func NewClient(s3 API, v bool) *Client {
+	return &Client{
+		s3api:   s3,
+		verbose: v,
+	}
 }
 
-func (c *client) DeleteBucketContents(bucket *string) ([]*s3.DeletedObject, error) {
+// DeleteBucketContents will remove all object versions and delete markers from the specified s3 bucket.
+func (c *Client) DeleteBucketContents(bucket string) ([]*s3.DeletedObject, error) {
 	deletedObjects := make([]*s3.DeletedObject, 0)
 
 	for {
 		// 2 API calls are needed to delete things in s3, the first will get a list
 		// of the objects in the bucket, and the second to delete those things
-		listResponse, listErr := c.s3api.ListObjectVersions(&s3.ListObjectVersionsInput{
-			Bucket: bucket,
-		})
-		if listErr != nil {
-			return nil, errors.Wrap(listErr, "error listing object versions on bucket")
+		objIDs, objIDErr := c.GetObjectIdentifiers(bucket)
+		if objIDErr != nil {
+			return nil, errors.Wrap(objIDErr, "package: s3svc => method: DeleteBucketContents => method call s3svc.Client.GetObjectIdentifiers failed\n")
 		}
 
-		// svc.ListObjectVersions will only return 1000 results at a time
-		numObjects := len(listResponse.DeleteMarkers) + len(listResponse.Versions)
-
 		// delete things until there's nothing left
-		if numObjects == 0 {
+		if len(objIDs) == 0 {
 			return deletedObjects, nil
 		}
 
-		// We're emptying the bucket, no need to discrimiate the things to delete
-		var items s3.Delete
-		var objectIdentifiers = make([]*s3.ObjectIdentifier, numObjects)
-
-		for i, obj := range listResponse.Versions {
-			objectIdentifiers[i] = &s3.ObjectIdentifier{
-				Key:       obj.Key,
-				VersionId: obj.VersionId,
-			}
-		}
-
-		for i, obj := range listResponse.DeleteMarkers {
-			objectIdentifiers[i+len(listResponse.Versions)] = &s3.ObjectIdentifier{
-				Key:       obj.Key,
-				VersionId: obj.VersionId,
-			}
-		}
-
-		items.SetObjects(objectIdentifiers)
-
-		// DeleteObjects can only delete 1000 things at a time
-		deleteResponse, deleteErr := c.s3api.DeleteObjects(&s3.DeleteObjectsInput{
-			Bucket: bucket,
-			Delete: &items,
-		})
+		deleteIDs, deleteErr := c.DeleteObjects(bucket, objIDs)
 		if deleteErr != nil {
-			return nil, errors.Wrap(deleteErr, "error deleting objects")
+			return nil, errors.Wrap(deleteErr, "package: s3svc => method: DeleteBucketContents => method call s3svc.Client.DeleteObjects failed\n")
 		}
 
-		deletedObjects = append(deletedObjects, deleteResponse.Deleted...)
+		deletedObjects = append(deletedObjects, deleteIDs...)
 	}
 }
 
-func (c *client) ListBucketContents(bucket *string) error {
-	resp, _ := c.s3api.ListObjects(&s3.ListObjectsInput{
-		Bucket: bucket,
+// GetObjectIdentifiers returns object versions, delete markers, or both, from the specified s3 bucket.
+func (c *Client) GetObjectIdentifiers(bucket string) ([]*s3.ObjectIdentifier, error) {
+	resp, err := c.s3api.ListObjectVersions(&s3.ListObjectVersionsInput{Bucket: &bucket})
+	if err != nil {
+		return nil, errors.Wrap(err, "package: s3svc => method: GetObjectIdentifiers => method call s3api.ListObjectVersions failed\n")
+	}
+
+	objectIdentifiers := make([]*s3.ObjectIdentifier, 0)
+
+	if resp.Versions != nil {
+		objVersions := make([]*s3.ObjectIdentifier, len(resp.Versions))
+		for i, obj := range resp.Versions {
+			objVersions[i] = &s3.ObjectIdentifier{
+				Key:       obj.Key,
+				VersionId: obj.VersionId,
+			}
+		}
+
+		objectIdentifiers = append(objectIdentifiers, objVersions...)
+	}
+
+	if resp.DeleteMarkers != nil {
+		deadObjects := make([]*s3.ObjectIdentifier, len(resp.DeleteMarkers))
+		for i, obj := range resp.DeleteMarkers {
+			deadObjects[i] = &s3.ObjectIdentifier{
+				Key:       obj.Key,
+				VersionId: obj.VersionId,
+			}
+		}
+
+		objectIdentifiers = append(objectIdentifiers, deadObjects...)
+	}
+
+	return objectIdentifiers, nil
+}
+
+// DeleteObjects deletes the specified objects in an s3 bucket, including delete markers.
+func (c *Client) DeleteObjects(bucket string, objects []*s3.ObjectIdentifier) ([]*s3.DeletedObject, error) {
+	var deleteList s3.Delete
+
+	deleteList.SetObjects(objects)
+
+	deleteResponse, deleteErr := c.s3api.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: &bucket,
+		Delete: &deleteList,
 	})
+	if deleteErr != nil {
+		return nil, errors.Wrap(deleteErr, "package: s3svc => method: DeleteObjects => method call s3api.DeleteObjects failed\n")
+	}
 
-	fmt.Println(resp.Contents)
-
-	return nil
+	return deleteResponse.Deleted, nil
 }
